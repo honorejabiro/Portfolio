5ddcf477ab81d4dbb321be36cbd89094433d15a2
Honore Jabiro <honorejabiro@gmail.com>
2025-07-12 16:41:19 -0500

    Changed my resume

--- FullStack-Article/blog_bn/blog_bn/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('articles/', views.articles, name="articles"),
-    path('articles/<slug:slug>', views.article_detail, name="article_detail"),
-    path('article-search/', views.search, name="search")
-]
